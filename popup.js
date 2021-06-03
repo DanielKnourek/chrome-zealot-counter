bb83f92eb721b4c03b6526ca1e485d602709d4bb
DanielKnourek <dev.danielknourek@gmail.com>
2021-06-03 02:23:00 +0200

    basic version, proof of concept

--- popup.js
+++ popup.js
@@ -4,16 +4,9 @@ ZealotCountdiv.textContent = ZealotCount
 document.body.appendChild(ZealotCountdiv)
 
 document.addEventListener('DOMContentLoaded', function(){
-    chrome.tabs.query({currentWindow: true, active: true}, 
-        function(tabs){
-            chrome.tabs.sendMessage(tabs[0].id, 'hi', setCount)
-        })
+    process()
         
-    function setCount(res){
-        ZealotCount = parseInt(res.count)
-        ZealotCountdiv.textContent = `Zealots killed: ${ZealotCount} <br> in this sessin: ${ZealotCount - parseInt(localStorage.getItem("counterStart"))}`
 
-    }
     document.getElementById('setCounterStart').addEventListener('click', function (){
         setCounterStart(ZealotCount)
     }, false)
@@ -22,4 +15,82 @@ document.addEventListener('DOMContentLoaded', function(){
 
 function setCounterStart(value) {
     localStorage.setItem("counterStart", value); 
+}
+
+function setCount(res){
+    ZealotCount = parseInt(res.count)
+    ZealotCountdiv.innerHTML = "";
+    ZealotCountdiv.appendChild(document.createTextNode(`Zealots killed: ${ZealotCount} `));
+    ZealotCountdiv.appendChild(document.createElement('br'));
+    ZealotCountdiv.appendChild(document.createTextNode(`in this sessin: ${ZealotCount - parseInt(localStorage.getItem("counterStart"))}`));
+
+}
+
+async function process(){
+
+    const api_link = "https://api.hypixel.net" 
+    const params_ = {
+        key : 'badde73b-850a-4027-bda9-8a753c50e245',
+        uuid: '2cac5693716a404dae32e3f4107c6c1a'
+    }
+
+    let querry = `${api_link}/player${strinfyParams(params_)}`;
+    console.log(querry)
+
+    await fetch(querry)
+        .then(result => result.json())
+        .then(({ player }) => {
+            // Log the player's username
+            console.log(player.displayname)
+
+            for (const key in player.stats.SkyBlock.profiles) {
+                if(player.stats.SkyBlock.profiles[key].cute_name == "Grapes"){
+                    params_.profile = player.stats.SkyBlock.profiles[key].profile_id
+                    break;
+                }
+            }
+        })
+    // console.log(params_.profile)
+    
+    let account = undefined
+    
+    querry = `${api_link}/skyblock/profile${strinfyParams(params_)}`;
+    console.log(querry)
+
+    killCounter(querry, params_.uuid)    
+}
+
+async function killCounter(querry, uuid) {
+    let start = await getCount(querry, uuid);
+    printCurrent(start, start)
+    setCounterStart(start)
+
+    setInterval(async function(){
+        printCurrent(start, await getCount(querry, uuid))
+    }, 5000)
+}
+
+function printCurrent(start, current){
+    // console.log(`${start} | ${start-current}`)
+    setCount({count: current})
+}
+
+async function getCount(querry, uuid){
+    let account = {}
+    await fetch(querry)
+    .then(result => result.json())
+    .then(({ profile }) => {
+        account = profile.members[uuid]        
+    })
+
+    // console.log(account.stats.kills_zealot_enderman)
+    return account.stats.kills_zealot_enderman;
+}
+
+function strinfyParams(params_) {
+    let stringified = new Array()
+    for (const key in params_) {        
+        stringified.push(`${key}=${params_[key]}`);
+    }
+    return `?${stringified.join("&")}`
 }
